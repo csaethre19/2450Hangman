27088548bbf2bc37f3cb2c773ba208a487227104
Charlotte <csaethre@bruinmail.slcc.edu>
2020-09-24 20:50:18 -0600

    sets up first scene: gallows and shelves

---
+++ Hangman2450/Assets/FreakOutCan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreakOutCan : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+    }
+}
