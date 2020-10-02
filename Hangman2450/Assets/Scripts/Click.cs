73dfec9411cd27ed9d5ec7339140e2e972adaf4b
csaethre19 <56099138+csaethre19@users.noreply.github.com>
2020-10-01 20:45:16 -0600

    models and scripts
    broken code for cody to fix

---
+++ Hangman2450/Assets/Scripts/Click.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public LayerMask clickableLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayHit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickableLayer))
+            {
+                rayHit.collider.GetComponent<FreakOutCan>().Click();
+            }
+        }
+    }
+}
---
+++ Hangman2450/Assets/Scripts/Clue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+//using TMPro;
+using UnityEditorInternal;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Clue : MonoBehaviour
+{
+    //public Text clue;
+    private string word = "unity";
+    private char[] clueLetters;
+    void Start()
+    {
+        clueLetters = new char[word.Length];
+        for (int i = 0; i < word.Length; i++)
+        {
+            clueLetters[i] = '_';
+        }
+        PrintClue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PrintClue();
+    }
+
+    private void PrintClue()
+    {
+        string updatedClue = "";
+        for (int i = 0; i < clueLetters.Length; i++)
+        {
+            updatedClue += clueLetters.ElementAt(i) + " ";
+        }
+        //clue.text = updatedClue;
+    }
+
+    public void ModifyClue(char letter)
+    {
+        char[] letters = word.ToCharArray();
+        for (int i = 0; i < letters.Length; i++)
+        {
+            if (letters.ElementAt(i) == letter)
+            {
+                clueLetters[i] = letter;
+            }
+        }
+        
+    }
+
+    public Boolean CheckGuess(char letter)
+    {
+        if (word.Contains(letter.ToString()))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
