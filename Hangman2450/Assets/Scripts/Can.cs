a93c190a8d9a8144eb282e1442f5e811ced8640a
CodiushMaximush <codysalmond@gmail.com>
2020-10-19 14:08:49 -0600

    Feedback Variety, Particles, Sounds and Terrain
    - Cans now jump back with random force between minThrust and maxThrust when clicked
    - Smoke Particles now spawn where you click
    - Sparks Particles now spawn when you click on a can
    - Ping sound when you hit a can
    - Moved Gunshot Sound to Camera Object
    - Organized Game Hierarchy
    - Added Terrain
    - Added "Sun"
    - Made Dave's Colors Match better

--- Hangman2450/Assets/Scripts/Can.cs
+++ Hangman2450/Assets/Scripts/Can.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.EventSystems;
 
-public class Can : MonoBehaviour
-{
+public class Can : MonoBehaviour {
     public Rigidbody rb;
-    public float thrust;
+    public float minThrust;
+    public float maxThrust;
     public char letter;
 
+    //audio fields
+    AudioSource pingSound;
+    public float minVolume;
+    public float maxVolume;
+    [Space(20)]
+    public float minPitch;
+    public float maxPitch;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        pingSound = gameObject.GetComponent<AudioSource>();
     }
 
-    public void Click()
-    {
-        rb.AddForce(transform.up * -thrust);
+    public void Click() {
+        rb.AddForce(transform.up * -Random.Range(minThrust, maxThrust));
         Debug.Log("Clicked: " + letter);
         CustomGameEventSystem.BroadcastGuessLetter(letter);
+
+        pingSound.volume = Random.Range(minVolume, maxVolume);
+        pingSound.pitch = Random.Range(minPitch, maxPitch);
+        pingSound.Play();
+
     }
-    
 }
